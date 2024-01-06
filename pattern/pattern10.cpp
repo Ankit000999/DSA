8039f0960f8845bdb926c57aae58b206be5a01c5
Ankit000999 <151570051+Ankit000999@users.noreply.github.com>
2024-01-06 16:38:04 +0530

    ....

---
+++ pattern/pattern10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+        int num=1;
+         int n=5;
+         for ( int i=0;i<n;i++) 
+         {
+        if( 1%2 == 1 ) num=1;
+        
+         else (num=0); 
+     
+     	for(int j=0; j<=i; j++)
+     	{
+     	  cout<<num ;
+     	 num=1-num ;
+     	}
+     	cout<<endl;}
+     	 
+     	 return 0;
+}
